4e4578631a0983da51c4f7afee70d7bd8998eeb6
Carriane Linzer <carrianelinzer@icloud.com>
2020-03-04 01:09:05 +0000

    first commit

---
+++ lib/traversal.rb
@@ -0,0 +1,50 @@
+class Tree
+  attr_accessor :payload, :children
+
+  def initialize(payload, children)
+    @payload = payload
+    @children = children
+  end
+end
+
+def depth_first(node, search_value)
+  # puts node.payload # Output to verifiy search order
+  return node if node.payload == search_value
+  node.children.each do |child|
+    depth_first(child, search_value)
+  end
+end
+
+def breadth_first(node, search_value)
+  queue = []
+  queue.push(node)
+
+  while queue.length != 0
+    n = queue.shift
+    # puts n.payload # Output to verifiy search order
+    return n if n.payload == search_value
+    n.children.each do |child|
+      queue.push(child)
+    end
+  end
+
+end
+
+# The "Leafs" of a tree, elements that have no children
+deep_fifth_node = Tree.new(5, [])
+eleventh_node   = Tree.new(11, [])
+fourth_node     = Tree.new(4, [])
+
+# The "Branches" of the tree
+ninth_node         = Tree.new(9, [fourth_node])
+sixth_node         = Tree.new(6, [deep_fifth_node, eleventh_node])
+seventh_node       = Tree.new(7, [sixth_node])
+shallow_fifth_node = Tree.new(5, [ninth_node])
+
+# The "Trunk" of the tree
+trunk   = Tree.new(2, [seventh_node, shallow_fifth_node])
+
+puts "Depth First:"
+depth_first(trunk, 11)
+puts "Breadth First:"
+breadth_first(trunk, 11)
